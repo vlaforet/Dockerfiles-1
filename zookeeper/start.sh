c981989ad6a68185b8692ca511471c86db9bddb3
CoreOS Admin <me@vivi7865.com>
2015-09-12 10:01:04 +0000

    extrasme/zookeeper Use workdir for configuration (removing $ZOOKEEPER_DIR)

--- zookeeper/start.sh
+++ zookeeper/start.sh
@@ -1,21 +1,25 @@
 #! /bin/sh
 
-cp $ZOOKEEPER_DIR/conf/zoo.cfg.template $ZOOKEEPER_DIR/conf/zoo.cfg
+cp conf/zoo.cfg.template conf/zoo.cfg
+
 OIFS=$IFS
 IFS=','
+
 ID=1
 for server in $ZK_SERVERS
 do
-  echo "server.$ID=$server:2888:3888" >> $ZOOKEEPER_DIR/conf/zoo.cfg
+  echo "server.$ID=$server:2888:3888" >> conf/zoo.cfg
   ID=$(expr $ID + 1)
 done
+
 IFS=$OIFS
-cat $ZOOKEEPER_DIR/conf/zoo.cfg
 
+cat conf/zoo.cfg
+echo $JMXDISABLE
 
-echo "Building myid as server $ZK_ID"
+echo "Building myid as server dsq $ZK_ID"
 echo $ZK_ID > /data/myid
 echo "Starting zookeeper server $(cat /data/myid)"
 export JMXDISABLE=true
 
-#$ZOOKEEPER_DIR/bin/zkServer.sh start-foreground
+./bin/zkServer.sh start-foreground
