c5d85abb1783d1c938f9c8ef297cba6353af7600
vivi7865 <me@vivi7865.com>
2015-11-01 19:17:34 +0000

    Cleaned a little mesos-slave-autodisc

--- mesos/mesos-slave-docker-autodisc/discovery.sh
+++ mesos/mesos-slave-docker-autodisc/discovery.sh
@@ -6,6 +6,8 @@ echo "Container ip $IP_ADDR"
 echo "Searching for zookeepers in etcd, can take a moment"
 
 export ZK_SERVERS=$(curl -L http://$ETCD_ENDPOINT:$ETCD_PORT/v2/keys/$REGISTRATOR/$ZK_IMAGE_NAME-$ZK_PORT/ --silent | jq -r '.node.nodes | map(.value) | unique | join(",")')
-export MASTER=zk://$ZK_SERVERS/mesos
 
-./start.sh --ip=$IP_ADDR $*
+export MESOS_MASTER=zk://$ZK_SERVERS/mesos
+export MESOS_IP=*IP_ADDR
+
+./start.sh $*
