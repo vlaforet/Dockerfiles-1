afeee22aeba52a7c3de40de3cabb120b88b239ea
vivi7865 <me@vivi7865.com>
2015-12-05 21:48:25 +0000

    Elasticsearch clustering

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-/elasticsearch/bin/elasticsearch $*
+if [ "$1" = "master" ]; then
+  echo "node.data=false" >> /elasticsearch/config/elasticsearch.yml
+fi
+
+if [ -z "$2" ]; then
+  CMD_ARGS="-Des.cluster.name=$2"
+fi
+
+/elasticsearch/bin/elasticsearch $CMD_ARGS
 
